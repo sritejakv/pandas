65e40c4c689865bcd53ed6c6dfd6de3f80836c07
Kummita Sriteja <sriteja.ku@gmail.com>
2020-02-19 19:18:37 +0100

    Adding pycallgraph

---
+++ py_call_graph.py
@@ -0,0 +1,21 @@
+import pytest
+from pycallgraph import PyCallGraph
+from pycallgraph.output import GraphvizOutput
+from pycallgraph.config import Config
+from pycallgraph.globbing_filter import GlobbingFilter
+
+
+@pytest.fixture(autouse=True)
+def record_call_graph(request):
+    import time
+    current_millis = int(round(time.time() * 1000))
+    with PyCallGraph(output=GraphvizOutput(dot_file='./dotFiles/%s.dot' % current_millis),
+                     config=Config(debug=True,
+                                   trace_filter=GlobbingFilter(
+                                       exclude=['pycallgraph.*', '*.tests.*', '*.yml', "_pytest.*", "numpy.*",
+                                                "pandas._libs.*", "pandas._config.*", "pandas.conftest.*",
+                                                "pandas.testing.*", "pandas._testing.*"],
+                                       include=['pandas.*']
+                                   )
+                                   )):
+        yield
